0450e1668bd84cc61d720b612d4763c8480df5cb
Deyvid Dimitrov <davida1991@gmail.com>
2026-05-02 02:57:13 +0300

    SEO: Switch to BrowserRouter, add sitemap.xml, robots.txt and fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Lenis from 'lenis'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
